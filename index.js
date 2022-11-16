a485614c8096e5e92a1454dbff7acb4482ebc338
JosineiGrau <jgrausalazar@gmail.com>
2022-11-16 18:21:48 -0500

    hola

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const app = require('./src/app');
-// if (process.env.NODE_ENV !== 'production') {
-// 	require('dotenv').config();
-// }
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config();
+}
 
-// const PORT = process.env.PORT || 3030;
-// console.log(process.env.PORT);
+const PORT = process.env.PORT || 3030;
 
-const server = app.listen(8080, () => {
+const server = app.listen(PORT, () => {
 	console.log(`mi servidor: http://localhost:${server.address().port}`);
 });
